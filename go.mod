926efd57a4d11bef32c8de0aee35ead928505f0b
weiraneve <907221539@qq.com>
2025-12-04 15:57:33 +0800

    feat: hash password

--- go.mod
+++ go.mod
@@ -1,14 +1,13 @@
 module hello-demo
 
-go 1.23.2
+go 1.24.0
 
 require (
-	github.com/sashabaranov/go-openai v1.40.5
+	golang.org/x/crypto v0.45.0
 	gopkg.in/yaml.v2 v2.4.0
 )
 
 require (
-	github.com/golang-jwt/jwt/v4 v4.5.2 // indirect
 	github.com/kr/pretty v0.3.1 // indirect
 	github.com/rogpeppe/go-internal v1.10.0 // indirect
 	gopkg.in/check.v1 v1.0.0-20201130134442-10cb98267c6c // indirect
--- hello.go
+++ hello.go
@@ -1,57 +1,15 @@
 package main
 
 import (
-	"crypto/ecdsa"
-	"crypto/elliptic"
-	"crypto/rand"
-	"crypto/x509"
-	"encoding/base64"
 	"fmt"
-	"math/big"
-	mrand "math/rand"
-	"time"
-
-	"github.com/golang-jwt/jwt/v4"
+	"golang.org/x/crypto/bcrypt"
 )
 
-func makeCert() (*ecdsa.PrivateKey, string) {
-	key, _ := ecdsa.GenerateKey(elliptic.P256(), rand.Reader)
-	tmpl := &x509.Certificate{
-		SerialNumber:          new(big.Int).SetInt64(time.Now().UnixNano()),
-		NotBefore:             time.Now().Add(-time.Hour),
-		NotAfter:              time.Now().Add(24 * time.Hour),
-		BasicConstraintsValid: true,
-	}
-	der, _ := x509.CreateCertificate(rand.Reader, tmpl, tmpl, &key.PublicKey, key)
-	return key, base64.StdEncoding.EncodeToString(der)
-}
-
-func sign(key *ecdsa.PrivateKey, x5c string, claims jwt.MapClaims) string {
-	token := jwt.NewWithClaims(jwt.SigningMethodES256, claims)
-	token.Header["x5c"] = []string{x5c}
-	s, _ := token.SignedString(key)
-	return s
-}
-
 func main() {
-	key, x5c := makeCert()
-	tx := fmt.Sprintf("tx-%d", mrand.Int63())
-
-	onetimeClaims := jwt.MapClaims{
-		"bundleId":      "com.example.local",
-		"environment":   "Sandbox",
-		"productId":     "purchase_basic",
-		"transactionId": tx,
-		"iat":           time.Now().Unix(),
+	plainPassword := "fanyo_1234"
+	hashed, err := bcrypt.GenerateFromPassword([]byte(plainPassword), bcrypt.DefaultCost)
+	if err != nil {
+		// handle error
 	}
-	fmt.Println("ONETIME JWS:\n", sign(key, x5c, onetimeClaims))
-
-	subClaims := jwt.MapClaims{
-		"bundleId":      "com.example.local",
-		"environment":   "Sandbox",
-		"productId":     "subscription_premium",
-		"transactionId": fmt.Sprintf("tx-%d", mrand.Int63()),
-		"iat":           time.Now().Unix(),
-	}
-	fmt.Println("SUB JWS:\n", sign(key, x5c, subClaims))
+	fmt.Println(string(hashed))
 }
